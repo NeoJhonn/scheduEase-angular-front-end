b2d51358acefc5502802cd221b35cd985414b4b9
Neojhonn <jhonny.azevedo@gmail.com>
2023-10-06 18:26:52 -0300

    createEmployee method created in employee service

--- src/app/services/create-employee.service.ts
+++ src/app/services/create-employee.service.ts
@@ -6,12 +6,28 @@ import { Observable } from 'rxjs';
   providedIn: 'any'
 })
 export class CreateEmployeeService {
-  private apiUrl = 'https://scheduease-production.up.railway.app/api';
+  private apiUrl = 'https://scheduease-production.up.railway.app/api/employees';
+  employee: any = {};
+  
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    
+  }
 
 
   listEmployees(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/employees/list-all`);
+    return this.http.get<any>(`${this.apiUrl}/list-all`);
+  }
+
+  createEmployee(employee: any = {}): void {
+    this.http.post(this.apiUrl, employee)
+      .subscribe({
+        next: (response) => {
+          console.log('Funcionário cadastrado com sucesso:', response);
+        },
+        error: (error) => {
+          console.error('Erro ao cadastrar funcionário:', error);
+        }
+      });
   }
 }
