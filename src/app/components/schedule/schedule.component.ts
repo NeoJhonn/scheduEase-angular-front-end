12edf47dbb80013d7a0b1cc4f3aa436a57fc4060
NeoJhonn <jhonny.azevedo@gmail.com>
2023-10-14 02:03:28 -0300

    Validations in Schedule page finaly done :fireworks:

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -5,30 +5,48 @@ import { Schedule } from 'src/app/@core/models/schedule.model';
 const ELEMENT_DATA: Schedule[] = [
   {
     id: 1,
-    clientName: 'Fabiana da Silva',
+    clientName: 'Vanusa',
     employeeId: 1,
-    serviceBooked: 'mechas',
+    serviceBooked: 'botox',
     appointmentDate: '2023-10-09',
     startTime: '9:00',
     endTime: '11:30'
   },
   {
     id: 1,
-    clientName: 'José Figueredo',
+    clientName: 'Carlos da Silva',
     employeeId: 1,
-    serviceBooked: 'corte',
+    serviceBooked: 'botox',
     appointmentDate: '2023-10-09',
     startTime: '13:30',
     endTime: '14:00'
   },
   {
     id: 1,
-    clientName: 'Rosana souza',
+    clientName: 'Vini filho Flávio',
+    employeeId: 1,
+    serviceBooked: 'botox',
+    appointmentDate: '2023-10-09',
+    startTime: '14:30',
+    endTime: '14:30'
+  },
+  {
+    id: 1,
+    clientName: 'Rosimeri Andrade',
     employeeId: 1,
     serviceBooked: 'botox',
     appointmentDate: '2023-10-09',
     startTime: '17:00',
     endTime: '19:30'
+  },
+  {
+    id: 1,
+    clientName: 'Lu Carioca',
+    employeeId: 1,
+    serviceBooked: 'pé e mão',
+    appointmentDate: '2023-10-09',
+    startTime: '20:30',
+    endTime: '22:00'
   }
 
 ];
@@ -47,9 +65,6 @@ export class ScheduleComponent implements OnInit {
   '22:00']
   displayedColumns: string[] = ['time', 'name', 'service', 'action'];
   dataSource = this.timeGrid;
-  public count: number = 0;
-  public control: boolean =  false;
-
   public elements = ELEMENT_DATA;
 
   constructor() { }
@@ -62,38 +77,148 @@ export class ScheduleComponent implements OnInit {
   isFree(hour: string, elements: Schedule[]): boolean{
     let isFree: boolean = true;
     let intervals: String[] = [];
-   
     let indexS: number = -1;
     let indexE: number = -1;
 
+    // Pegar o elemento que tem um horário da vez do loop
     for (let el of elements) {
-      if (hour !== el.startTime){
+      if (this.timeGrid.indexOf(hour) >= this.timeGrid.indexOf(el.startTime)){
+        // pegas os dois index do intevalo de hora marcada
         indexS = this.timeGrid.indexOf(el.startTime);
         indexE = this.timeGrid.indexOf(el.endTime);
+
       }
     }
 
-
-    
-
-    console.log(indexS);
-    console.log(indexE);
+    if (indexE === -1 && indexS === -1) {
+      isFree = true;
+    } else {
+      if ( indexS === indexE) {
+        intervals.push(this.timeGrid[indexS]);
+        // seta falso nesse intervalo para econder o botão Agendar
+      for (let h of intervals) {
+        if (this.timeGrid.indexOf(hour) === this.timeGrid.indexOf(h)) {
+          isFree = false;
+        }
+      }
+      } else {
+    // popula o array com os horário do intevalo
     for (let i= indexS; i <= indexE; i++) {
       intervals.push(this.timeGrid[i]);
     }
 
+    // seta falso nesse intervalo para econder o botão Agendar
     for (let h of intervals) {
       if (this.timeGrid.indexOf(hour) === this.timeGrid.indexOf(h)) {
         isFree = false;
       }
     }
+  }
+  }
 
-    console.log(intervals);
-    
 
     return isFree;
   }
 
-  
+  setReserved(hour: string, elements: Schedule[]): boolean{
+    let isFree: boolean = true;
+    let intervals: String[] = [];
+    let indexS: number = -1;
+    let indexE: number = -1;
+
+    // Pegar o elemento que tem um horário da vez do loop
+    for (let el of elements) {
+      if (this.timeGrid.indexOf(hour) >= this.timeGrid.indexOf(el.startTime)){
+        // pegas os dois index do intevalo de hora marcada
+        indexS = this.timeGrid.indexOf(el.startTime)+1;
+        indexE = this.timeGrid.indexOf(el.endTime);
+
+      }
+    }
+
+    if (indexE === -1 && indexS === -1) {
+      isFree = true;
+    } else {
+      if ( indexS === indexE) {
+        intervals.push(this.timeGrid[indexS]);
+        // seta falso nesse intervalo para econder o botão Agendar
+      for (let h of intervals) {
+        if (this.timeGrid.indexOf(hour) === this.timeGrid.indexOf(h)) {
+          isFree = false;
+        }
+      }
+      } else {
+    // popula o array com os horário do intevalo
+    for (let i= indexS; i <= indexE; i++) {
+      intervals.push(this.timeGrid[i]);
+    }
+
+    // seta falso nesse intervalo para econder o botão Agendar
+    for (let h of intervals) {
+      if (this.timeGrid.indexOf(hour) === this.timeGrid.indexOf(h)) {
+        isFree = false;
+      }
+    }
+  }
+  }
+
+
+    return isFree;
+  }
+
+  setReserved2(hour: string, elements: Schedule[]): boolean{
+    let isFree: boolean = true;
+    let intervals: String[] = [];
+    let indexS: number = -1;
+    let indexE: number = -1;
+
+    // Pegar o elemento que tem um horário da vez do loop
+    for (let el of elements) {
+      if (this.timeGrid.indexOf(hour) >= this.timeGrid.indexOf(el.startTime)){
+        // pegas os dois index do intevalo de hora marcada
+        indexS = this.timeGrid.indexOf(el.startTime);
+        indexE = this.timeGrid.indexOf(el.endTime);
+
+      }
+    }
+
+    if (indexE === -1 && indexS === -1) {
+      isFree = true;
+    } else {
+      if ( indexS === indexE) {
+        intervals.push(this.timeGrid[indexS]);
+        // seta falso nesse intervalo para econder o botão Agendar
+      for (let h of intervals) {
+        if (this.timeGrid.indexOf(hour) === this.timeGrid.indexOf(h)) {
+          isFree = false;
+        }
+      }
+      } else {
+    // popula o array com os horário do intevalo
+    for (let i= indexS; i <= indexE; i++) {
+      intervals.push(this.timeGrid[i]);
+    }
+
+    // seta falso nesse intervalo para econder o botão Agendar
+    for (let h of intervals) {
+      if (this.timeGrid.indexOf(hour) === this.timeGrid.indexOf(h)) {
+        isFree = false;
+      }
+    }
+  }
+  }
+
+
+    return isFree;
+  }
+
+  isIndexEquals(a: number, b: number): boolean {
+    if (a === b) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
 
 }
